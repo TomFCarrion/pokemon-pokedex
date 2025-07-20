2a380643237a08ee37930b68688887363e636847
Tomas F. Carrion <tomas.carrion@academyex.com>
2025-07-20 12:13:11 +1200

    chore: initial project setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ["raw.githubusercontent.com"],
+    formats: ["image/webp", "image/avif"],
+  },
+  experimental: {
+    optimizePackageImports: ["styled-components"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
